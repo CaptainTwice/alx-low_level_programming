9102c95b84c115d2ed5001363fb214bc92b1dd38
CaptainTwice <ibitoye166@gmail.com>
2022-09-10 04:09:47 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+nclude <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
