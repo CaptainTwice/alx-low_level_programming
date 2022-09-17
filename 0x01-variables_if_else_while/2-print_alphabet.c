8e290873f77d434ca3ac5719ef85359c09d87847
CaptainTwice <ibitoye166@gmail.com>
2022-09-17 13:57:37 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.o>
+
+/**
+ * main - Entry point
+ *
+ * return: 0
+ */
+int main(void)
+
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a)
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
