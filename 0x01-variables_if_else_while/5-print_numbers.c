6588ac03f5bc50ea0cdc73c16f28e8711359bafa
CaptainTwice <ibitoye166@gmail.com>
2022-09-17 15:54:22 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(v0id)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%i", a);
+	printf("\n");
+	return (0);
+
+}
