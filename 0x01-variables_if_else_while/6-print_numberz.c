9b9769debe697e4a12ee8c5abe3c5a1c9bdddb8c
CaptainTwice <ibitoye166@gmail.com>
2022-09-17 16:10:19 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+in main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
