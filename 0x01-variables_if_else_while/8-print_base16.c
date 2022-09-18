4c39c36cb7763b506b282f1846f5bc0f77fbe432
CaptainTwice <ibitoye166@gmail.com>
2022-09-18 06:01:49 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i =48; i < 58; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f', i++)
+		putchar(i);
+	putcharr('\n');
+	return (0);
+}
