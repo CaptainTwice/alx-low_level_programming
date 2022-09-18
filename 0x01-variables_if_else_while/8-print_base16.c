983a28ec5b2387d107017176ae0d1d9047d2eda0
ibitoye oladimeji <110991520+CaptainTwice@users.noreply.github.com>
2022-09-18 14:08:10 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i;
 
-	for (i =48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 		putchar(i);
-	for (i = 'a'; i <= 'f', i++)
+	for (i = 'a'; i <= 'f'; i++)
 		putchar(i);
 	putcharr('\n');
 	return (0);
